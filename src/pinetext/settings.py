533480052eea1238435ad76d8942b9796a3bb0e7
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-07-25 19:59:36 -0700

    Add Pinecone settings

--- src/pinetext/settings.py
+++ src/pinetext/settings.py
@@ -1,8 +1,15 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class Pinecone(BaseModel):
+    api_key: str | None = None
+    assistant: str | None = None
+    model: str | None = None
+
+
 class Settings(BaseSettings):
-    pinecone_key: str | None = None
+    pinecone: Pinecone = Pinecone()
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
